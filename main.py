09b09851cca85f01b1aec35841902e7432456840
fuzz88 <ivan@oschepkov.ru>
2025-04-28 14:59:30 +0800

    add phantom_read

--- main.py
+++ main.py
@@ -5,36 +5,39 @@ import logging
 from threading import Thread, Event
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
 
 def main():
-    conn = connect(connection_factory=LoggingConnection,
-    dbname = "postgres",
-    user = "postgres",
-    host = "localhost",
-    password = "postgres"
-    )
-
-    conn.initialize(logger)
-    conn.autocommit = False
-
-    print("\npsycopg2.extensions:")
-    for attr in dir(extensions):
-        if "ISOLATION" in attr:
-            print(f"    {attr}")
-    print()
-
-    conn.set_isolation_level(extensions.ISOLATION_LEVEL_READ_UNCOMMITTED)
+    conn = connect(dbname="postgres",
+                   user="postgres",
+                   host="localhost",
+                   password="postgres"
+                   )
 
     create_table(conn)
-    dirty_read(conn)
-    unrepeatable_read(conn)
+    print("-----------------dirty_read with READ_COMMITTED")
+    dirty_read(extensions.ISOLATION_LEVEL_READ_COMMITTED)
+    drop_table(conn)
+    create_table(conn)
+    print("-----------------unrepeatable_read with READ_COMMITTED")
+    unrepeatable_read(extensions.ISOLATION_LEVEL_READ_COMMITTED)
+    drop_table(conn)
+    create_table(conn)
+    print("-----------------unrepeatable_read with REPEATABLE_READ")
+    unrepeatable_read(extensions.ISOLATION_LEVEL_REPEATABLE_READ)
+    drop_table(conn)
+    create_table(conn)
+    print("-----------------phantom_read with READ_COMMITTED")
+    phantom_read(extensions.ISOLATION_LEVEL_READ_COMMITTED)
+    drop_table(conn)
+    create_table(conn)
+    print("-----------------phantom_read with REPEATABLE_READ")
+    phantom_read(extensions.ISOLATION_LEVEL_REPEATABLE_READ)
     drop_table(conn)
 
     conn.close()
 
 
-def dirty_read(conn):
+def dirty_read(isolation_level):
     sync_event = Event()
 
     def thread1_func():
@@ -46,19 +49,16 @@ def dirty_read(conn):
             host="localhost",
             password="postgres"
         )
+        logger = logging.getLogger("thread1")
         conn1.initialize(logger)
         conn1.autocommit = False
-        conn1.set_isolation_level(extensions.ISOLATION_LEVEL_READ_UNCOMMITTED)
+        conn1.set_isolation_level(isolation_level)
         cursor = conn1.cursor()
         cursor.execute("BEGIN;")
         cursor.execute("UPDATE cars SET max_speed = 250 WHERE model = 'Toyota';")
-        print("Thread 1: Updated Toyota's max_speed to 250.")
-        # cursor.execute("COMMIT;")
-        sleep(1)
         sync_event.set()  # Signal thread2 to proceed
-        sleep(3)
+        sleep(1)
         cursor.execute("COMMIT;")
-        print("Thread 1: Transaction committed.")
         cursor.close()
         conn1.close()
 
@@ -71,9 +71,10 @@ def dirty_read(conn):
             host="localhost",
             password="postgres"
         )
+        logger = logging.getLogger("thread2")
         conn2.initialize(logger)
         conn2.autocommit = False
-        conn2.set_isolation_level(extensions.ISOLATION_LEVEL_READ_UNCOMMITTED)
+        conn2.set_isolation_level(isolation_level)
         sync_event.wait()  # Wait for thread1 to signal
         cursor = conn2.cursor()
         cursor.execute("BEGIN;")
@@ -94,9 +95,9 @@ def dirty_read(conn):
 
     thread1.join()
     thread2.join()
- 
 
-def unrepeatable_read(conn):
+
+def unrepeatable_read(isolation_level):
     sync_event = Event()
 
     def thread1_func():
@@ -108,9 +109,10 @@ def unrepeatable_read(conn):
             host="localhost",
             password="postgres"
         )
+        logger = logging.getLogger("thread1")
         conn1.initialize(logger)
         conn1.autocommit = False
-        conn1.set_isolation_level(extensions.ISOLATION_LEVEL_REPEATABLE_READ)
+        conn1.set_isolation_level(isolation_level)
         cursor = conn1.cursor()
         cursor.execute("BEGIN;")
         cursor.execute("SELECT * FROM cars WHERE model='Renault';")
@@ -119,7 +121,7 @@ def unrepeatable_read(conn):
         for row in rows:
             print(f"    {row}")
         sync_event.set()  # Signal thread2 to proceed
-        sleep(3)  # Wait for thread2 to commit changes
+        sleep(1)  # Wait for thread2 to commit changes
         cursor.execute("SELECT * FROM cars WHERE model='Renault';")
         rows = cursor.fetchall()
         print("Thread 1: Second read:")
@@ -138,14 +140,79 @@ def unrepeatable_read(conn):
             host="localhost",
             password="postgres"
         )
+        logger = logging.getLogger("thread2")
         conn2.initialize(logger)
         conn2.autocommit = False
-        conn2.set_isolation_level(extensions.ISOLATION_LEVEL_REPEATABLE_READ)
+        conn2.set_isolation_level(isolation_level)
         sync_event.wait()  # Wait for thread1 to signal
         cursor = conn2.cursor()
         cursor.execute("BEGIN;")
         cursor.execute("UPDATE cars SET max_speed = max_speed + 30 WHERE model='Renault';")
-        print("Thread 2: Updated Renault's max_speed by 30.")
+        cursor.execute("COMMIT;")
+        cursor.close()
+        conn2.close()
+
+    thread1 = Thread(target=thread1_func)
+    thread2 = Thread(target=thread2_func)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+
+def phantom_read(isolation_level):
+    sync_event = Event()
+
+    def thread1_func():
+        # Create a new connection for thread1
+        conn1 = connect(
+            connection_factory=LoggingConnection,
+            dbname="postgres",
+            user="postgres",
+            host="localhost",
+            password="postgres"
+        )
+        logger = logging.getLogger("thread1")
+        conn1.initialize(logger)
+        conn1.autocommit = False
+        conn1.set_isolation_level(isolation_level)
+        cursor = conn1.cursor()
+        cursor.execute("BEGIN;")
+        cursor.execute("SELECT * FROM cars;")
+        rows = cursor.fetchall()
+        print("Thread 1: Initial read:")
+        for row in rows:
+            print(f"    {row}")
+        sync_event.set()  # Signal thread2 to proceed
+        sleep(1)  # Wait for thread2 to commit changes
+        cursor.execute("SELECT * FROM cars;")
+        rows = cursor.fetchall()
+        print("Thread 1: Second read:")
+        for row in rows:
+            print(f"    {row}")
+        cursor.execute("COMMIT;")
+        cursor.close()
+        conn1.close()
+
+    def thread2_func():
+        # Create a new connection for thread2
+        conn2 = connect(
+            connection_factory=LoggingConnection,
+            dbname="postgres",
+            user="postgres",
+            host="localhost",
+            password="postgres"
+        )
+        logger = logging.getLogger("thread2")
+        conn2.initialize(logger)
+        conn2.autocommit = False
+        conn2.set_isolation_level(isolation_level)
+        sync_event.wait()  # Wait for thread1 to signal
+        cursor = conn2.cursor()
+        cursor.execute("BEGIN;")
+        cursor.execute("DELETE FROM cars WHERE model = 'Renault';")
         cursor.execute("COMMIT;")
         cursor.close()
         conn2.close()
@@ -165,8 +232,10 @@ def drop_table(conn):
         cursor.execute("DROP TABLE IF EXISTS cars;")
         conn.commit()
 
+
 def create_table(conn):
     with conn.cursor() as cursor:
+        cursor.execute("DROP TABLE IF EXISTS cars;")
         cursor.execute("""CREATE TABLE IF NOT EXISTS cars (
 id BIGSERIAL PRIMARY KEY,
 model varchar(20) UNIQUE,
@@ -178,9 +247,6 @@ max_speed INTEGER);""")
 ('Renault', 170),
 ('Mercedes', 230);""")
         conn.commit()
-
-
-            
 
 
 if __name__ == "__main__":
